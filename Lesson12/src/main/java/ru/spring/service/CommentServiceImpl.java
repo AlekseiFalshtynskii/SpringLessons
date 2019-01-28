fc57f69945db55071394848da8c10021bc04b0b6
AlekseiFalshtynskii <aleksei.falshtynskii@gmail.com>
2019-01-28 23:03:36 +0300

    fix

--- Lesson12/src/main/java/ru/spring/service/CommentServiceImpl.java
+++ Lesson12/src/main/java/ru/spring/service/CommentServiceImpl.java
@@ -2,6 +2,7 @@ package ru.spring.service;
 
 import org.springframework.stereotype.Service;
 import ru.spring.model.Comment;
+import ru.spring.repository.BookRepository;
 import ru.spring.repository.CommentRepository;
 
 import java.util.List;
@@ -9,13 +10,17 @@ import java.util.List;
 @Service
 public class CommentServiceImpl implements CommentService {
     private final CommentRepository repository;
+    private final BookRepository bookRepository;
 
-    public CommentServiceImpl(CommentRepository repository) {
+    public CommentServiceImpl(CommentRepository repository,
+                              BookRepository bookRepository) {
         this.repository = repository;
+        this.bookRepository = bookRepository;
     }
 
     @Override
     public String save(Comment comment) {
+        comment.setBook(bookRepository.findById(comment.getBook().getId()).orElse(null));
         return repository.save(comment).getId();
     }
 
